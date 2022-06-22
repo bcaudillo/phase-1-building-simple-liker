82c6d1b7a7fd5408268237bb5d09a433cfac72a0
bcaudillo <btcaudillo@gmail.com>
2022-06-21 23:16:41 -0600

    commit tree 9504b401bfa216d983aad1795030f9fc4e6f978f

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+const errorModal = document.querySelector("#modal")
+errorModal.classList.add("hidden")
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM CONTENT HAS LOADED")
+})
+
+clickListener()
+
+function clickListener(){
+  document.addEventListener("click",(event)=> {
+  console.log(event.target.classList)
+  console.log(event.target.classList[0])
+  if(event.target.classList[1]===('activated-heart')){
+    mimicServerCall()
+    .then(resp=>{
+      event.target.classList.remove("activated-heart")
+    })
+    .catch(error=>{
+      errorModal.classList.add("hidden")
+      setTimeout(()=>{hideError()}, 3000)
+    }
+  )}
+  else if(event.target.classList[0] === 'like-glyph'){
+    mimicServerCall()
+    .then(resp=>{
+      event.target.classList.add("activated-heart")
+        })
+    .catch(error=>{
+      errorModal.classList.remove("hidden")
+      setTimeout(()=>{hideError()}, 3000)
+    }
+      )}
+}
+  )}
 
 
-
+//When a user clicks on an empty heart: Invoke mimicServerCall
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
